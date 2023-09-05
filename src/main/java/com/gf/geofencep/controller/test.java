8fa255e5795af3c2ec26d2c8b99ae37572e0f418
urim <urim.ahmetaj@crownpeak.com>
2023-09-05 21:45:27 +0200

    change 2

--- src/main/java/com/gf/geofencep/controller/test.java
+++ src/main/java/com/gf/geofencep/controller/test.java
@@ -4,7 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("Hello, World! changed");
-        System.out.println("Hello, World! 1");
+        System.out.println("Hello, World! 1 changed");
         System.out.println("Hello, World! 2");
 
 
