68bd2a8a8f256db42e7a77eb04b63956bf6dfd95
urim <urim.ahmetaj@crownpeak.com>
2023-09-05 21:51:05 +0200

    Squashed commit of the following:
    commit ccfdbc3aac306ea6d92291be42cce131e7216499
    Author: urim <urim.ahmetaj@crownpeak.com>
    Date:   Tue Sep 5 21:49:59 2023 +0200
    
        r
    
    commit b6add29d2883ca30e9aff289e3ffc07ae45d88c1
    Author: urim <urim.ahmetaj@crownpeak.com>
    Date:   Tue Sep 5 21:49:47 2023 +0200
    
        2
    
    commit d2b80f37ac9b7dd82cba59a056fdb93c08b11dfe
    Author: urim <urim.ahmetaj@crownpeak.com>
    Date:   Tue Sep 5 21:49:38 2023 +0200
    
        1

--- src/main/java/com/gf/geofencep/controller/test.java
+++ src/main/java/com/gf/geofencep/controller/test.java
@@ -3,18 +3,9 @@ package com.gf.geofencep.controller;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World! changed");
-        System.out.println("Hello, World! 1 changed");
-        System.out.println("Hello, World! 2");
-
-
-        System.out.println("Hello, World! 2-0");
-        System.out.println("Hello, World! 3-0");
-        System.out.println("Hello, World! 4-0");
-
-        System.out.println("Hello, World! A");
-        System.out.println("Hello, World! B");
-        System.out.println("Hello, World! C");
+        System.out.println("changed, print!");
+        System.out.println("e, print!");
+        System.out.println("r, print!");
 
 
 
