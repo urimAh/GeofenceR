6101561e9b7c982006b08c911296fb9876f6a471
urim <urim.ahmetaj@crownpeak.com>
2023-09-05 21:45:17 +0200

    change 1
    change 2
    
    change A
    
    change B
    
    change C

--- src/main/java/com/gf/geofencep/controller/test.java
+++ src/main/java/com/gf/geofencep/controller/test.java
@@ -3,14 +3,21 @@ package com.gf.geofencep.controller;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World! 1");
+        System.out.println("Hello, World! changed");
+        System.out.println("Hello, World! 1 changed");
         System.out.println("Hello, World! 2");
 
 
         System.out.println("Hello, World! 2-0");
         System.out.println("Hello, World! 3-0");
         System.out.println("Hello, World! 4-0");
+
+        System.out.println("Hello, World! A");
+        System.out.println("Hello, World! B");
+        System.out.println("Hello, World! C");
+
+
+
 
 
     }
